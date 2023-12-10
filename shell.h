e721d028cc36106941d6ba007bf930bab3d6c2f1
HajarElMannani <laso125@hotmail.com>
2023-12-10 02:04:54 -0800

    shell.h

---
+++ shell.h
@@ -0,0 +1,24 @@
+#ifndef SIMSH_H
+#define SIMSH_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+int _strlen(char *s);
+int _strcmp(const char *s1, const char *s2);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *fullPath(char *command);
+int pathexist(char *command);
+char *_strdup(const char *str);
+char *_getenv(const char *name);
+void _env(void);
+void intToStr(int num, char *str);
+#endif /*SIMSH_H*/
